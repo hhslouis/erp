7ce29f16b6ecfea88f181da313ea0652d2aae4c5
annytecsoft@gmail.com <annytecsoft@gmail.com@25885026-f3a8-8e73-4669-efa9af965c3a>
2014-10-24 19:52:29 +0000

    jorge

--- sampu/JavaSource/paq_facturacion/pre_factura.java
+++ sampu/JavaSource/paq_facturacion/pre_factura.java
@@ -14,6 +14,18 @@ public class pre_factura extends 	Pantalla{
 		// TODO Auto-generated constructor stub
 		tab_factura.setId("tab_factura");
 		tab_factura.setTabla("fac_factura", "ide_fafac", 1);
+		tab_factura.setTipoFormulario(true);
+		tab_factura.getGrid().setColumns(4);
+		//tab_factura.getColumna("ide_comov").setCombo("cont_movimiento", "ide_comov", "detalle_asiento_comov", "");
+		tab_factura.getColumna("ide_comov").setVisible(false);
+		tab_factura.getColumna("ide_fadaf").setCombo("fac_datos_factura", "ide_fadaf", "serie_factura_fadaf", "");
+		tab_factura.getColumna("ide_gtemp").setCombo("gth_empleado", "ide_gtemp", "documento_identidad_gtemp", "");
+		tab_factura.getColumna("ide_retip").setCombo("rec_tipo", "ide_retip", "detalle_retip", "");
+		//tab_factura.getColumna("ide_tedar").setCombo("tes_datos_retencion", "ide_tedar", "tipo_retencion_tedar", "");
+		tab_factura.getColumna("ide_tedar").setVisible(false);
+		tab_factura.getColumna("ide_retip").setCombo("rec_tipo", "ide_retip", "detalle_retip", "");
+		tab_factura.getColumna("ide_faesf").setCombo("fac_estado_factura", "ide_faesf", "detalle_faesf", "");
+		tab_factura.getColumna("ide_recli").setCombo("select ide_recli,nombre_comercial_recli from rec_clientes order by nombre_comercial_recli");
 		tab_factura.dibujar();
 		tab_factura.agregarRelacion(tab_detalle_factura);
 		
