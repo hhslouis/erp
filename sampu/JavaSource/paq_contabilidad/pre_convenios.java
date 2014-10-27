d12073fb8b2763227095364e9ae99af9769465e3
sanfly89 <sanfly89@gmail.com>
2014-10-27 21:59:09 +0000

    jorge

--- sampu/JavaSource/paq_contabilidad/pre_convenios.java
+++ sampu/JavaSource/paq_contabilidad/pre_convenios.java
@@ -20,7 +20,7 @@ public class pre_convenios extends Pantalla {
     private Tabla tab_tabla1 = new Tabla();
     private Tabla tab_tabla2 = new Tabla();
     private Tabla tab_tabla3 = new Tabla();
-    private Tabla tab_tabla4 = new Tabla();
+
     private Arbol arb_arbol = new Arbol();
     
     
@@ -38,8 +38,8 @@ public class pre_convenios extends Pantalla {
         tab_tabla1.getColumna("ide_cotic").setCombo("cont_tipo_consumo","ide_cotic","detalle_cotic","");
 		tab_tabla1.getColumna("ide_geins").setCombo("gen_institucion","ide_geins","detalle_geins","");
 		tab_tabla1.getColumna("ide_coest").setCombo("cont_estado","ide_coest","detalle_coest","");
-        tab_tabla1.agregarRelacion(tab_tabla2);
-        tab_tabla1.agregarRelacion(tab_tabla3);
+       // tab_tabla1.agregarRelacion(tab_tabla2);
+       // tab_tabla1.agregarRelacion(tab_tabla3);
         tab_tabla1.agregarArbol(arb_arbol);
         tab_tabla1.dibujar();
         PanelTabla pat_panel1 = new PanelTabla();
@@ -51,14 +51,23 @@ public class pre_convenios extends Pantalla {
         tab_tabla2.setId("tab_tabla2");
         tab_tabla2.setIdCompleto("tab_tabulador:tab_tabla2");
         tab_tabla2.setTabla("cont_responsable_convenio", "ide_corec", 2);
-        
+        tab_tabla2.getColumna("ide_cocon").setCombo("cont_convenio", "ide_cocon", "detalle_contrato_cocon", "");
+        tab_tabla2.getColumna("ide_getip").setCombo("gen_tipo_persona", "ide_getip", "detalle_gtip", "");
+     	tab_tabla2.getColumna("ide_gecaf").setCombo("gen_cargo_funcional", "ide_gecaf", "detalle_gecaf", "");
+     	//tab_tabla2.getColumna("ide_geedp").setCombo("gen_empleados_departamento_par", "ide_geedp", "", "");
+        tab_tabla2.setTipoFormulario(true);  //formulario
+        tab_tabla1.getGrid().setColumns(4);
         tab_tabla2.dibujar();
         PanelTabla pat_panel2 = new PanelTabla();
         pat_panel2.setPanelTabla(tab_tabla2);
 
         tab_tabla3.setId("tab_tabla3");
         tab_tabla3.setIdCompleto("tab_tabulador:tab_tabla3");
+        tab_tabla3.setTipoFormulario(true);;  //formulario 
+        tab_tabla3.getGrid().setColumns(4);
         tab_tabla3.setTabla("cont_archivo_convenio", "ide_coarc", 3);
+        tab_tabla3.getColumna("foto_coarc").setUpload("fotos");
+    	tab_tabla3.getColumna("foto_coarc").setImagen("128", "128");
         tab_tabla3.dibujar();
         PanelTabla pat_panel3 = new PanelTabla();
         pat_panel3.setPanelTabla(tab_tabla3);
@@ -131,11 +140,5 @@ public class pre_convenios extends Pantalla {
         this.tab_tabla3 = tab_tabla3;
     }
 
-    public Tabla getTab_tabla4() {
-        return tab_tabla4;
-    }
-
-    public void setTab_tabla4(Tabla tab_tabla4) {
-        this.tab_tabla4 = tab_tabla4;
-    }
+  
 }
