d8130286b87c34db9fae65f0fbfb5c2e335811e4
sanfly89 <sanfly89@gmail.com>
2014-10-27 17:04:26 +0000

    jorge

--- sampu/JavaSource/paq_contabilidad/pre_convenios.java
+++ sampu/JavaSource/paq_contabilidad/pre_convenios.java
@@ -1,137 +1,141 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package paq_contabilidad;
 
+import paq_sistema.aplicacion.Pantalla;
 import framework.componentes.Arbol;
-import framework.componentes.AutoCompletar;
 import framework.componentes.Division;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
 import framework.componentes.Tabulador;
-import paq_sistema.aplicacion.Pantalla;
 
-public class pre_convenios extends Pantalla{
-	 
-	 private Tabla tab_responsable_conv =new Tabla();
-	 private Tabla tab_archivo_conv=new Tabla();	
-	private Tabla tab_convenio=new Tabla();
-		
-	private Arbol arb_convenio=new Arbol();
-		
-	public pre_convenios(){
-	
-		Tabulador tab_tabulador = new Tabulador();
+/**
+ *
+ * @author Diego
+ */
+public class pre_convenios extends Pantalla {
+
+    private Tabla tab_tabla1 = new Tabla();
+    private Tabla tab_tabla2 = new Tabla();
+    private Tabla tab_tabla3 = new Tabla();
+    private Tabla tab_tabla4 = new Tabla();
+    private Arbol arb_arbol = new Arbol();
+    
+    
+
+    public pre_convenios() {
+    	Tabulador tab_tabulador = new Tabulador();
         tab_tabulador.setId("tab_tabulador");
-                
-						        
-		tab_convenio.setId("tab_convenio");
-		tab_convenio.setTipoFormulario(true);  //formulario 
-		tab_convenio.getGrid().setColumns(4); //hacer  columnas 
-		tab_convenio.setTabla("cont_convenio", "ide_cocon", 1);
-		
-		
-	    tab_convenio.getColumna("ide_cotic").setCombo("cont_tipo_consumo","ide_cotic","detalle_cotic","");
-		tab_convenio.getColumna("ide_geins").setCombo("gen_institucion","ide_geins","detalle_geins","");
-		tab_convenio.getColumna("ide_coest").setCombo("cont_estado","ide_coest","detalle_coest","");
-	 
-	   	   
-		// contruccion del arbol
-		tab_convenio.setCampoPadre("con_ide_cocon"); //necesarios para el arbol ide recursivo
-		tab_convenio.agregarArbol(arb_convenio);//necesarios para el arbol
-		tab_convenio.setCampoNombre("(select ide_cocon||' '|| detalle_contrato_cocon as detalle_contrato_cocon from cont_convenio b where b.ide_cocon=cont_convenio.ide_cocon)"); //necesarios para el arbol campo a mostrarse
-		
-		tab_convenio.dibujar();
-				
-		PanelTabla pat_convenio = new PanelTabla();
-		pat_convenio.setPanelTabla(tab_convenio);
 
-		arb_convenio.setId("arb_convenio");
-		arb_convenio.dibujar();
-		Division division=new Division();
-		division.dividir1(arb_convenio);
-		tab_responsable_conv.setId("tab_responsable_conv");
-		tab_responsable_conv.setIdCompleto("tab_tabulador:tab_responsable_conv");
-		tab_responsable_conv.setTabla("cont_responsable_convenio","ide_corec",2);
-		tab_responsable_conv.dibujar();
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setTabla("cont_convenio", "ide_cocon", 1);
+        tab_tabla1.setTipoFormulario(true);  //formulario 
+        tab_tabla1.getGrid().setColumns(4); //hacer  columnas 
+        tab_tabla1.setCampoPadre("con_ide_cocon");
+        tab_tabla1.setCampoNombre("detalle_contrato_cocon");
+        tab_tabla1.getColumna("ide_cotic").setCombo("cont_tipo_consumo","ide_cotic","detalle_cotic","");
+		tab_tabla1.getColumna("ide_geins").setCombo("gen_institucion","ide_geins","detalle_geins","");
+		tab_tabla1.getColumna("ide_coest").setCombo("cont_estado","ide_coest","detalle_coest","");
+        tab_tabla1.agregarRelacion(tab_tabla2);
+        tab_tabla1.agregarRelacion(tab_tabla3);
+        tab_tabla1.agregarArbol(arb_arbol);
+        tab_tabla1.dibujar();
+        PanelTabla pat_panel1 = new PanelTabla();
+        pat_panel1.setPanelTabla(tab_tabla1);
+
+        arb_arbol.setId("arb_arbol");
+        arb_arbol.dibujar();
+
+        tab_tabla2.setId("tab_tabla2");
+        tab_tabla2.setIdCompleto("tab_tabulador:tab_tabla2");
+        tab_tabla2.setTabla("cont_responsable_convenio", "ide_corec", 2);
+        
+        tab_tabla2.dibujar();
         PanelTabla pat_panel2 = new PanelTabla();
-        pat_panel2.setPanelTabla(tab_responsable_conv);
-        
-        tab_archivo_conv.setId("tab_archivo_conv");
-		tab_archivo_conv.setIdCompleto("tab_tabulador:tab_archivo_conv");
-		tab_archivo_conv.setTabla("cont_archivo_convenio","ide_coarc",2);
-		tab_archivo_conv.dibujar();
+        pat_panel2.setPanelTabla(tab_tabla2);
+
+        tab_tabla3.setId("tab_tabla3");
+        tab_tabla3.setIdCompleto("tab_tabulador:tab_tabla3");
+        tab_tabla3.setTabla("cont_archivo_convenio", "ide_coarc", 3);
+        tab_tabla3.dibujar();
         PanelTabla pat_panel3 = new PanelTabla();
-        pat_panel3.setPanelTabla(tab_archivo_conv);
-	
-        tab_tabulador.agregarTab("RESPONSABLE", pat_panel2);//intancia los tabuladores
-        tab_tabulador.agregarTab("ARCHIVO", pat_panel3);
+        pat_panel3.setPanelTabla(tab_tabla3);
+
         
-        Division div_division=new Division();
-		div_division.dividir2(arb_convenio,tab_tabulador,"50%","H");
-		//division.dividir3(arb_convenio, pat_panel2, pat_panel3,"25%", "50%", "h");
-		agregarComponente(div_division);
-		
 
-	}
-	
-	@Override
-	public void insertar() {
-		// TODO Auto-generated method stub
-		tab_convenio.insertar();
-		
-	}
+        tab_tabulador.agregarTab("RESPONSABLE CONVENIO",pat_panel2);
+        tab_tabulador.agregarTab("ARCHIVO CONVENIO", pat_panel3);
+       
 
-	@Override
-	public void guardar() {
-		// TODO Auto-generated method stub
-		if(tab_convenio.guardar()){
-            guardarPantalla();
-            //actualiza el arbol
-            arb_convenio.ejecutarSql();
-            utilitario.addUpdate("arb_arbol");
+        Division div3 = new Division(); //UNE OPCION Y DIV 2
+        div3.dividir2(pat_panel1, tab_tabulador, "60%", "H");
+        Division div_division = new Division();
+        div_division.setId("div_division");
+        div_division.dividir2(arb_arbol, div3, "21%", "V");  //arbol y div3
+        agregarComponente(div_division);
+    }
+
+    @Override
+    public void insertar() {
+        utilitario.getTablaisFocus().insertar();
+    }
+
+    @Override
+    public void guardar() {
+        if (tab_tabla1.guardar()) {
+            if (tab_tabla2.guardar()) {
+                if (tab_tabla3.guardar()) {
+                    
+                }
+            }
         }
-		
-				
-	}
+        guardarPantalla();
+    }
 
-	@Override
-	public void eliminar() {
-		// TODO Auto-generated method stub
-		tab_convenio.eliminar();
-		
-	}
-	
-	public Tabla getTab_responsable_conv() {
-		return tab_responsable_conv;
-	}
+    @Override
+    public void eliminar() {
+        utilitario.getTablaisFocus().eliminar();
+    }
 
-	public void setTab_responsable_conv(Tabla tab_responsable_conv) {
-		this.tab_responsable_conv = tab_responsable_conv;
-	}
+    public Arbol getArb_arbol() {
+        return arb_arbol;
+    }
 
-	public Tabla getTab_archivo_conv() {
-		return tab_archivo_conv;
-	}
+    public void setArb_arbol(Arbol arb_arbol) {
+        this.arb_arbol = arb_arbol;
+    }
 
-	public void setTab_archivo_conv(Tabla tab_archivo_conv) {
-		this.tab_archivo_conv = tab_archivo_conv;
-	}
+    public Tabla getTab_tabla1() {
+        return tab_tabla1;
+    }
 
-	public Tabla getTab_convenio() {
-		return tab_convenio;
-	}
+    public void setTab_tabla1(Tabla tab_tabla1) {
+        this.tab_tabla1 = tab_tabla1;
+    }
 
-	public void setTab_convenio(Tabla tab_convenio) {
-		this.tab_convenio = tab_convenio;
-	}
+    public Tabla getTab_tabla2() {
+        return tab_tabla2;
+    }
 
-	public Arbol getArb_convenio() {
-		return arb_convenio;
-	}
+    public void setTab_tabla2(Tabla tab_tabla2) {
+        this.tab_tabla2 = tab_tabla2;
+    }
 
-	public void setArb_convenio(Arbol arb_convenio) {
-		this.arb_convenio = arb_convenio;
-	}
-	
-	
-	
+    public Tabla getTab_tabla3() {
+        return tab_tabla3;
+    }
 
+    public void setTab_tabla3(Tabla tab_tabla3) {
+        this.tab_tabla3 = tab_tabla3;
+    }
+
+    public Tabla getTab_tabla4() {
+        return tab_tabla4;
+    }
+
+    public void setTab_tabla4(Tabla tab_tabla4) {
+        this.tab_tabla4 = tab_tabla4;
+    }
 }
