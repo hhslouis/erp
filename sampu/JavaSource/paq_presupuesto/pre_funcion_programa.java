0d019abea2a6ad70de67611015c7be89429b0755
sarybeata <sarybeata@gmail.com>
2014-11-12 16:47:01 +0000

    se creo la clase pre_funcion_programa

---
+++ sampu/JavaSource/paq_presupuesto/pre_funcion_programa.java
@@ -0,0 +1,61 @@
+package paq_presupuesto;
+
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import paq_sistema.aplicacion.Pantalla;
+
+public class pre_funcion_programa extends Pantalla {
+	private Tabla tab_funcion_programa=new Tabla();
+	public pre_funcion_programa (){
+		tab_funcion_programa.setId("tab_funcion_programa");
+		tab_funcion_programa.setNumeroTabla(1);
+		tab_funcion_programa.setTabla("pre_funcion_programa", "ide_prfup ", 1);
+		tab_funcion_programa.getColumna("ide_geani").setCombo("select ide_geani,detalle_geani from gen_anio order by detalle_geani");
+		//tab_funcion_programa.getColumna("pre_ide_prfup").setCombo("select ide_prfup, detalle_prfup from pre_funcion_programa order by detalle_prfup");
+		tab_funcion_programa.setTipoFormulario(true);
+		tab_funcion_programa.getGrid().setColumns(4);
+		
+		tab_funcion_programa.dibujar();
+		PanelTabla pat_funcion_programa=new PanelTabla();
+		pat_funcion_programa.setPanelTabla(tab_funcion_programa);
+		agregarComponente(pat_funcion_programa);
+		
+	}
+
+
+	@Override
+	public void insertar() {
+		// TODO Auto-generated method stub
+		tab_funcion_programa.insertar();
+		
+		
+	}
+
+	@Override
+	public void guardar() {
+		// TODO Auto-generated method stub
+		tab_funcion_programa.guardar();
+		guardarPantalla();
+		
+	}
+
+
+	@Override
+	public void eliminar() {
+		// TODO Auto-generated method stub
+		tab_funcion_programa.eliminar();
+		
+	}
+	
+	public Tabla getTab_funcion_programa() {
+		return tab_funcion_programa;
+	}
+
+
+	public void setTab_funcion_programa(Tabla tab_funcion_programa) {
+		this.tab_funcion_programa = tab_funcion_programa;
+	}
+
+
+
+}
