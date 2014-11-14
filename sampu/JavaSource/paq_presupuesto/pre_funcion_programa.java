ad63806a47503a50b43ddf0fdccd00130c6c4fe9
sarybeata <sarybeata@gmail.com>
2014-11-14 13:53:33 +0000

    modificación

--- sampu/JavaSource/paq_presupuesto/pre_funcion_programa.java
+++ sampu/JavaSource/paq_presupuesto/pre_funcion_programa.java
@@ -12,8 +12,8 @@ public class pre_funcion_programa extends Pantalla {
 		tab_funcion_programa.setTabla("pre_funcion_programa", "ide_prfup ", 1);
 		tab_funcion_programa.getColumna("ide_geani").setCombo("select ide_geani,detalle_geani from gen_anio order by detalle_geani");
 		//tab_funcion_programa.getColumna("pre_ide_prfup").setCombo("select ide_prfup, detalle_prfup from pre_funcion_programa order by detalle_prfup");
-		tab_funcion_programa.setTipoFormulario(true);
-		tab_funcion_programa.getGrid().setColumns(4);
+		//tab_funcion_programa.setTipoFormulario(true);
+		//tab_funcion_programa.getGrid().setColumns(4);
 		
 		tab_funcion_programa.dibujar();
 		PanelTabla pat_funcion_programa=new PanelTabla();
