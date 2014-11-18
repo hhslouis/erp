a8084acb2d72e46e91d0b84490a9b4fab5b80d7c
taniagc2788 <taniagc2788@gmail.com>
2014-11-18 21:53:36 +0000

    paq nuevo

--- sampu/JavaSource/paq_general/ejb/ServicioGeneral.java
+++ sampu/JavaSource/paq_general/ejb/ServicioGeneral.java
@@ -1,8 +1,34 @@
 package paq_general.ejb;
 
-import javax.ejb.Remote;
+import javax.ejb.Stateless;
 
-@Remote
-public interface ServicioGeneral {
+import framework.aplicacion.TablaGenerica;
+import paq_general.ejb.ServicioGeneral;
+import paq_sistema.aplicacion.Utilitario;
+
+@Stateless
+public  class ServicioGeneral  {
+	
+	private Utilitario utilitario= new Utilitario();
+	
+	public String getTipoPersona(String  pmodulo){
+		
+		String tab_tipo_persona= "select a.ide_getip,detalle_getip from gen_tipo_persona a, gen_tipo_persona_modulo b" +
+				" where a.ide_getip=b.ide_getip and ide_gemod in ( "+pmodulo+")";
+		
+		 return tab_tipo_persona;
+		
+	}
+	
+	public TablaGenerica getTablaTipoPersona(String modulo){
+		
+		TablaGenerica tab_tipo_persona=utilitario.consultar("select a.ide_getip,detalle_getip from gen_tipo_persona a, gen_tipo_persona_modulo b" +
+				" where a.ide_getip=b.ide_getip and ide_gemod in ("+modulo+")");
+		 return tab_tipo_persona;
+		 
+	}
+	
+	
+	
 
 }
