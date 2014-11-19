7dd40d6bb241b9318aa5a8de8ecf22ca175e94b4
jaimebermeo14@gmail.com <jaimebermeo14@gmail.com@25885026-f3a8-8e73-4669-efa9af965c3a>
2014-11-19 14:02:29 +0000

    Encabezado y detalle facturación

--- sampu/JavaSource/paq_facturacion/pre_estado_factura.java
+++ sampu/JavaSource/paq_facturacion/pre_estado_factura.java
@@ -6,31 +6,33 @@ import paq_sistema.aplicacion.Pantalla;
 
 public class pre_estado_factura extends Pantalla{
 
-	private Tabla tab_estado_factura=new Tabla();
+	private Tabla tab_cont_estado=new Tabla();
 
 
 	public pre_estado_factura(){
-		tab_estado_factura.setId("tab_estado_factura");
-		tab_estado_factura.setTabla("fac_estado_factura","ide_faesf", 1);
-		tab_estado_factura.dibujar();
+		tab_cont_estado.setHeader("FACTURACÓN");
+		tab_cont_estado.setId("tab_cont_estado");
+		tab_cont_estado.setTabla("cont_estado","ide_coest", 1);
+		tab_cont_estado.dibujar();
 		PanelTabla pat_estado_factura=new PanelTabla();
-		pat_estado_factura.setPanelTabla(tab_estado_factura);
+		pat_estado_factura.setPanelTabla(tab_cont_estado);
 
 		agregarComponente(pat_estado_factura);
+		
 	}
 
 
 	@Override
 	public void insertar() {
 		// TODO Auto-generated method stub
-		tab_estado_factura.insertar();
+		tab_cont_estado.insertar();
 
 	}
 
 	@Override
 	public void guardar() {
 		// TODO Auto-generated method stub
-		tab_estado_factura.guardar();
+		tab_cont_estado.guardar();
 		guardarPantalla();
 		
 
@@ -39,18 +41,18 @@ public class pre_estado_factura extends Pantalla{
 	@Override
 	public void eliminar() {
 		// TODO Auto-generated method stub
-		tab_estado_factura.eliminar();
+		tab_cont_estado.eliminar();
 
 	}
 
 
 	public Tabla getTab_estado_factura() {
-		return tab_estado_factura;
+		return tab_cont_estado;
 	}
 
 
 	public void setTab_estado_factura(Tabla tab_estado_factura) {
-		this.tab_estado_factura = tab_estado_factura;
+		this.tab_cont_estado = tab_estado_factura;
 	}
 
 }
