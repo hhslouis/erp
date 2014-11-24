b29a6c1b7697a48072f401decf7c6e0947fcf8cb
janneth20873 <janneth20873@gmail.com>
2014-11-24 20:26:14 +0000

    Metodo

--- sampu/JavaSource/paq_facturacion/pre_factura.java
+++ sampu/JavaSource/paq_facturacion/pre_factura.java
@@ -13,13 +13,13 @@ import paq_sistema.aplicacion.Pantalla;
 
 
 public class pre_factura extends 	Pantalla{
-	
+
 	private Tabla tab_factura = new Tabla();
 	private Tabla tab_detalle_factura=new Tabla();
 	private AutoCompletar aut_opciones=new AutoCompletar();
 	private Boolean tieneIvaProducto = new Boolean("true");
 
-	
+
 
 	public Boolean getTieneIvaProducto() {
 		return tieneIvaProducto;
@@ -59,10 +59,10 @@ public class pre_factura extends 	Pantalla{
 
 		tab_factura.dibujar();
 		tab_factura.agregarRelacion(tab_detalle_factura);
-		
+
 		PanelTabla pat_factura=new PanelTabla();
 		pat_factura.setPanelTabla(tab_factura);
-		
+
 		// TABLA 2
 		tab_detalle_factura.setId("tab_detalle_factura");
 		tab_detalle_factura.setTabla("fac_detalle_factura", "ide_fadef", 2);
@@ -72,27 +72,27 @@ public class pre_factura extends 	Pantalla{
 		tab_detalle_factura.getColumna("ide_bomat").setCombo("select ide_bomat,codigo_bomat,detalle_bomat,iva_bomat from bodt_material order by detalle_bomat");
 		tab_detalle_factura.getColumna("ide_bomat").setAutoCompletar();
 		tab_detalle_factura.dibujar();
-		
-		
+
+
 		PanelTabla pat_detalle_factura= new PanelTabla();
 		pat_detalle_factura.setMensajeWarn("DETALLE FACTURACIÓN");
 		pat_detalle_factura.setPanelTabla(tab_detalle_factura);
-		
-		
+
+
 		Division div_division=new Division();
 		div_division.dividir2(pat_factura, pat_detalle_factura, "50%", "h");
 		agregarComponente(div_division);
-		
+
 		//el auto completar
 		aut_opciones.setId("aut_opciones"); //id del componente
 		//Simpre el primer campo es el campo primario de la tabla, en este caso ide_opci
 		//El segundo campo es lo que va a visualizarse , en este caso los campos nom_opci,tipo_opci,paquete_opci
 		aut_opciones.setAutoCompletar("SELECT ide_fadaf,serie_factura_fadaf,autorizacion_fadaf,fecha_impresion_fadaf,fecha_vencimiento_fadaf " +
-		"FROM fac_datos_factura WHERE serie_factura_fadaf is not null order by serie_factura_fadaf");
+				"FROM fac_datos_factura WHERE serie_factura_fadaf is not null order by serie_factura_fadaf");
 		aut_opciones.setMetodoChange("seleccionoAutocompletar"); //ejecuta el metodo seleccionoAutocompletar
 		bar_botones.agregarComponente(new Etiqueta("Seleccione una opción :")); //Agrego etiqueta a la barra de botones
 		bar_botones.agregarComponente(aut_opciones);//Agrego autocompletar a la barra de botones
-		
+
 	}
 	//METDO AUTOCOMPLETAR
 	public void seleccionoAutocompletar(SelectEvent evt){
@@ -103,16 +103,16 @@ public class pre_factura extends 	Pantalla{
 		utilitario.agregarMensaje("VALOR", aut_opciones.getValor()); 
 		//muestra el nombre que selecciono
 		utilitario.agregarMensaje("NOMBRE", aut_opciones.getValorArreglo(1)); 
-		
+
 		tab_factura.setCondicion("ide_fadaf="+aut_opciones.getValor());
 		tab_factura.ejecutarSql();
 		//tab_factura.ejecutarValorForanea(val)
 		tab_detalle_factura.ejecutarValorForanea(tab_factura.getValorSeleccionado());
-		
+
 		//Tabla.setCondicion("Campo="+autocompletar.getvalorseleccionado)
 		//Tabla.ejecutarsql()
 		//Tabla2.ejecutarvalorforaneo(tabla.getvalotseleccionado)
-		}
+	}
 
 	public Tabla gettab_factura() {
 		return tab_factura;
@@ -129,27 +129,27 @@ public class pre_factura extends 	Pantalla{
 	public void settab_detalle_factura(Tabla tab_detalle_factura) {
 		this.tab_detalle_factura = tab_detalle_factura;
 	}
-	
+
 	// metodo tieneIvaProducto
 	private  boolean tieneIvaProducto(String ide_bodtmat){
 		//Declaramos un String con la consulta que vamos a ejecutar
 		String str_sql="Select * from bodt_material where ide_bomat="+ide_bodtmat;
 		//Asi se hacen consultas a la BDD
 		TablaGenerica tab_consulta=utilitario.consultar(str_sql);
-		
+
 		//Preguntamos si la tabla no esta vacia es decir que si retorno un resultado la consulta
-				if ( tab_consulta.isEmpty()==false) {
-				//Obtenemos el valor del campo y lo almacenamos en un String
-				String str_aplica_valor_bodmat= tab_consulta.getValor("aplica_valor_bodmat");
-				//Preguntamos si el valor de la variable es true
-				if(str_aplica_valor_bodmat!=null && str_aplica_valor_bodmat.equals("true")){
+		if ( tab_consulta.isEmpty()==false) {
+			//Obtenemos el valor del campo y lo almacenamos en un String
+			String str_aplica_valor_bodmat= tab_consulta.getValor("aplica_valor_bodmat");
+			//Preguntamos si el valor de la variable es true
+			if(str_aplica_valor_bodmat!=null && str_aplica_valor_bodmat.equals("true")){
 				return true; //Si carga iva
-				}
-				}
+			}
+		}
 		return false;  //retorna false
-		
+
 	}
-	
+
 
 	@Override
 	public void insertar() {
@@ -158,15 +158,15 @@ public class pre_factura extends 	Pantalla{
 			tab_factura.insertar();
 			aut_opciones.limpiar();
 			utilitario.addUpdate("idAutocompletar");
-			}
+		}
 		else if(tab_detalle_factura.isFocus()){
 			tab_detalle_factura.insertar();
 			aut_opciones.limpiar();
 			utilitario.addUpdate("idAutocompletar");
 		}
-		
-		
-		
+
+
+
 	}
 
 	@Override
@@ -174,11 +174,11 @@ public class pre_factura extends 	Pantalla{
 		// TODO Auto-generated method stub
 		if(tab_factura.guardar()){
 			tab_detalle_factura.guardar();
-		
+
 		}
-		
+
 		guardarPantalla();
-	
+
 	}
 
 	@Override
@@ -186,18 +186,18 @@ public class pre_factura extends 	Pantalla{
 		// TODO Auto-generated method stub
 		if(tab_factura.isFocus()){
 			tab_factura.eliminar();
-			
-			
-	}
+
+
+		}
 		else if(tab_factura.isFocus()){
 			tab_factura.eliminar();
 
-			
-		}
-			
+
 		}
 
-	
-	
+	}
+
+
+
 
 }
