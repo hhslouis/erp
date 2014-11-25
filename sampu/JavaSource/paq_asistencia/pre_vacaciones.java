1415e47e93c1383117a4f125bdd195f4b557d88f
taniagc2788 <taniagc2788@gmail.com>
2014-11-25 21:24:40 +0000

    cambio de strin a integer

--- sampu/JavaSource/paq_asistencia/pre_vacaciones.java
+++ sampu/JavaSource/paq_asistencia/pre_vacaciones.java
@@ -960,7 +960,7 @@ public class pre_vacaciones extends Pantalla {
 				if (rep_reporte.isVisible()){
 					p_parametros=new HashMap();				
 					rep_reporte.cerrar();		
-					p_parametros.put("IDE_GEEDP",ide_geedp_activo);
+					p_parametros.put("IDE_GEEDP", Integer.parseInt(ide_geedp_activo));
 					p_parametros.put("titulo", " BIESS GERENCIA ADMINISTRATIVA - FINANCIERA DEPARTAMENTO DE TALENTO HUMANO VACACIONES");
 					sef_reporte.setSeleccionFormatoReporte(p_parametros, rep_reporte.getPath());						
 					sef_reporte.dibujar();
