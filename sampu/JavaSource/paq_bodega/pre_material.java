a1a409ac7b10bb9d24b56b9228cdad968a83ca6b
janneth20873@gmail.com <janneth20873@gmail.com@25885026-f3a8-8e73-4669-efa9af965c3a>
2015-01-09 16:34:03 +0000

    Tarifas

--- sampu/JavaSource/paq_bodega/pre_material.java
+++ sampu/JavaSource/paq_bodega/pre_material.java
@@ -4,9 +4,7 @@ package paq_bodega;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-
+import framework.componentes.Division;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
 import paq_sistema.aplicacion.Pantalla;
@@ -14,14 +12,16 @@ import paq_sistema.aplicacion.Pantalla;
 public class pre_material extends Pantalla{
 
 	private Tabla tab_material=new Tabla();
+	private Tabla tab_material_tarifa = new Tabla();
 	
 		
 	public pre_material() {
 		tab_material.setId("tab_material");  
 		tab_material.setTabla("bodt_material","ide_bomat", 1);	
+		tab_material.agregarRelacion(tab_material_tarifa); //CON ESTO LE DECIMOS Q TIENE RELACION
 		tab_material.setTipoFormulario(true);
 		tab_material.getGrid().setColumns(4);
-			    tab_material.getColumna("ide_bounm").setCombo("bodt_unidad_medida", "ide_bounm", "detalle_bounm", "");
+		tab_material.getColumna("ide_bounm").setCombo("bodt_unidad_medida", "ide_bounm", "detalle_bounm", "");
 		tab_material.getColumna("ide_botip").setCombo("bodt_tipo_producto", "ide_botip", "detalle_botip", "");
 		tab_material.getColumna("ide_bogrm").setCombo("bodt_grupo_material", "ide_bogrm", "detalle_bogrm", "");
 		tab_material.getColumna("foto_bomat").setUpload("fotos");
@@ -45,36 +45,71 @@ public class pre_material extends Pantalla{
 		PanelTabla pat_material=new PanelTabla();
 		pat_material.setPanelTabla(tab_material);
 		
-		agregarComponente(pat_material);
+		
+		tab_material_tarifa.setId("tab_material_tarifa");
+		tab_material_tarifa.setTabla("tes_material_tarifa", "ide_temat", 2);
+		tab_material_tarifa.setTipoFormulario(true);
+		tab_material_tarifa.getGrid().setColumns(2);
+		tab_material_tarifa.getColumna("ide_tetar").setCombo("tes_tarifas", "ide_tetar", "detalle_tetar", "");
+		tab_material_tarifa.dibujar();
+		PanelTabla pat_material_tarifa= new PanelTabla();
+		pat_material_tarifa.setPanelTabla(tab_material_tarifa);
+		
+		Division div_division = new Division();
+		div_division.dividir2(pat_material, pat_material_tarifa, "50%", "H");
+		agregarComponente(div_division);
 		
 	}
+
+
+	
+
+	
+
+	public Tabla getTab_material() {
+		return tab_material;
+	}
+
+
+	public void setTab_material(Tabla tab_material) {
+		this.tab_material = tab_material;
+	}
+
+
+	public Tabla getTab_material_tarifa() {
+		return tab_material_tarifa;
+	}
+
+	public void setTab_material_tarifa(Tabla tab_material_tarifa) {
+		this.tab_material_tarifa = tab_material_tarifa;
+	}
+
+
+
+
+
 
 	@Override
 	public void insertar() {
 		// TODO Auto-generated method stub
-		tab_material.insertar();
+		utilitario.getTablaisFocus().insertar();
 	}
+
 
 	@Override
 	public void guardar() {
 		// TODO Auto-generated method stub
-		tab_material.guardar();
-		guardarPantalla();		
-		
+		if(tab_material.guardar()){
+			if(tab_material_tarifa.guardar()){
+				guardarPantalla();
+			}
+		}
 	}
 
 	@Override
 	public void eliminar() {
 		// TODO Auto-generated method stub
-		tab_material.eliminar();
-	}
-
-	public Tabla gettab_material() {
-		return tab_material;
-	}
-
-	public void settab_material(Tabla tab_material) {
-		this.tab_material = tab_material;
+		utilitario.getTablaisFocus().eliminar();
 	}
 
 
