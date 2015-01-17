d250cc5bf7cdf4dd17821a5ae6f263b7811d233b
sarybeata <sarybeata@gmail.com>
2015-01-17 16:15:03 +0000

    se creo la pantalla asiento contable pre_movimientos

---
+++ sampu/JavaSource/paq_contabilidad/pre_movimiento.java
@@ -0,0 +1,86 @@
+package paq_contabilidad;
+
+import paq_sistema.aplicacion.Pantalla;
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+
+public class pre_movimiento extends Pantalla{
+	
+	private Tabla tab_movimiento=new Tabla();
+	private Tabla tab_detalle_movimiento=new Tabla();
+	
+	public pre_movimiento (){
+		tab_movimiento.setId("tab_movimiento");
+		tab_movimiento.setHeader("MOVIMIENTOS");
+		tab_movimiento.setTabla("cont_movimiento", "ide_comov", 1);
+		tab_movimiento.getColumna("ide_cotim").setCombo("cont_tipo_movimiento", "ide_cotim", "detalle_cotim", "");
+		tab_movimiento.getColumna("ide_cotia").setCombo("cont_tipo_asiento", "ide_cotia", "detalle_cotia", "");
+		tab_movimiento.getColumna("ide_geare").setCombo("gen_area", "ide_geare", "detalle_geare", "");
+		tab_movimiento.setTipoFormulario(true);
+		tab_movimiento.getGrid().setColumns(4);
+		tab_movimiento.agregarRelacion(tab_detalle_movimiento);
+		tab_movimiento.dibujar();
+		PanelTabla pat_movimiento=new PanelTabla();
+		pat_movimiento.setPanelTabla(tab_movimiento);
+		
+		/////detalle movinto
+		tab_detalle_movimiento.setId("tab_detalle_movimiento");
+		tab_detalle_movimiento.setHeader("DETALLE DE MOVIMIENTO");
+		tab_detalle_movimiento.setTabla("cont_detalle_movimiento", "ide_codem", 2);
+		tab_detalle_movimiento.setTipoFormulario(true);
+		tab_detalle_movimiento.getGrid().setColumns(4);
+		tab_detalle_movimiento.dibujar();
+		PanelTabla pat_detalle_movimiento=new PanelTabla();
+		pat_detalle_movimiento.setPanelTabla(tab_detalle_movimiento);
+		
+		Division div_division =new Division();
+		div_division.dividir2(pat_movimiento, pat_detalle_movimiento, "50%", "H");
+		agregarComponente(div_division);
+		
+		
+		
+	}
+
+	@Override
+	public void insertar() {
+		// TODO Auto-generated method stub
+		utilitario.getTablaisFocus().insertar();
+		
+		
+	}
+
+	@Override
+	public void guardar() {
+		// TODO Auto-generated method stub
+		if (tab_movimiento.guardar()){
+			tab_detalle_movimiento.guardar();
+		}
+		guardarPantalla();
+	}
+
+	@Override
+	public void eliminar() {
+		// TODO Auto-generated method stub
+		utilitario.getTablaisFocus().eliminar();
+		
+	}
+
+	public Tabla getTab_movimiento() {
+		return tab_movimiento;
+	}
+
+	public void setTab_movimiento(Tabla tab_movimiento) {
+		this.tab_movimiento = tab_movimiento;
+	}
+
+	public Tabla getTab_detalle_movimiento() {
+		return tab_detalle_movimiento;
+	}
+
+	public void setTab_detalle_movimiento(Tabla tab_detalle_movimiento) {
+		this.tab_detalle_movimiento = tab_detalle_movimiento;
+	}
+	
+
+}
