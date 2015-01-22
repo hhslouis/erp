2e8c60b2229555a4c3505829a9fb83da08be788d
sarybeata <sarybeata@gmail.com>
2015-01-22 23:29:50 +0000

    rectificación pantalla bodt_grupo material

--- sampu/JavaSource/paq_facturacion/pre_datos_factura.java
+++ sampu/JavaSource/paq_facturacion/pre_datos_factura.java
@@ -1,55 +1,32 @@
 package paq_facturacion;
 
-import org.primefaces.event.SelectEvent;
-
-import framework.componentes.AutoCompletar;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
 import paq_sistema.aplicacion.Pantalla;
 
 public class pre_datos_factura extends Pantalla{
-	
+ 	
 private Tabla tab_datos_factura=new Tabla();
-private AutoCompletar aut_opciones=new AutoCompletar();
+
+public pre_datos_factura (){
+	tab_datos_factura.setId("tab_datos_factura");
+	tab_datos_factura.setTabla("fac_datos_factura", "ide_fadaf", 1);
+	tab_datos_factura.getColumna("ide_sucu").setVisible(false);
+	tab_datos_factura.getColumna("ide_gedep").setVisible(false);
+	tab_datos_factura.getColumna("ide_bogrm").setCombo("select ide_bogrm,detalle_bogrm,autorizacion_sri_bogrm from bodt_grupo_material order by detalle_bogrm");		
+	tab_datos_factura.setTipoFormulario(true);
+	tab_datos_factura.getGrid().setColumns(4);
+	tab_datos_factura.dibujar();
+	PanelTabla pat_datos=new PanelTabla();
+	pat_datos.setPanelTabla(tab_datos_factura);
+	agregarComponente(pat_datos);
 	
-	
-	public AutoCompletar getAut_opciones() {
-	return aut_opciones;
 }
-
-public void setAut_opciones(AutoCompletar aut_opciones) {
-	this.aut_opciones = aut_opciones;
-}
-
-	public pre_datos_factura(){
-		tab_datos_factura.setHeader("DATOS DE LA FACTURACIÓN");
-		tab_datos_factura.setId("tab_datos_factura");
-		tab_datos_factura.setTabla("fac_datos_factura","ide_fadaf", 1);
-		tab_datos_factura.setTipoFormulario(true);
-		
-		//autocompletar
-		tab_datos_factura.getColumna("ide_gedep").setCombo("select ide_gedep,gen_ide_gedep,detalle_gedep from gen_departamento order by detalle_gedep");
-		tab_datos_factura.getColumna("ide_gedep").setAutoCompletar();
-				
-		tab_datos_factura.dibujar();
-		PanelTabla pat_datos_factura=new PanelTabla();
-		pat_datos_factura.setPanelTabla(tab_datos_factura);
-		
-		agregarComponente(pat_datos_factura);
-	}
-
-
-	public void seleccionoAutocompletar(SelectEvent evt){
-		//Cuando selecciona una opcion del autocompletar
-		aut_opciones.onSelect(evt);//siempre debe hacerse el onSelect(evt)
-		utilitario.agregarMensaje("VALOR", aut_opciones.getValor()); //muestra el valor que selecciono
-		utilitario.agregarMensaje("NOMBRE", aut_opciones.getValorArreglo(1)); //muestra el nombre que selecciono
-		}
-	
 	@Override
 	public void insertar() {
 		// TODO Auto-generated method stub
 		tab_datos_factura.insertar();
+		
 		
 	}
 
@@ -67,13 +44,11 @@ public void setAut_opciones(AutoCompletar aut_opciones) {
 		tab_datos_factura.eliminar();
 		
 	}
-
 	public Tabla getTab_datos_factura() {
 		return tab_datos_factura;
 	}
-
 	public void setTab_datos_factura(Tabla tab_datos_factura) {
 		this.tab_datos_factura = tab_datos_factura;
 	}
-
+	
 }
