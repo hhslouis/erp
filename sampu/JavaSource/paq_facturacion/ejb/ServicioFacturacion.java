2472ce4b8d44149df2adea1837d42adef17f7a19
sarybeata@gmail.com <sarybeata@gmail.com@25885026-f3a8-8e73-4669-efa9af965c3a>
2015-01-28 13:57:08 +0000

    se realizo cambios en ejb facturacion

--- sampu/JavaSource/paq_facturacion/ejb/ServicioFacturacion.java
+++ sampu/JavaSource/paq_facturacion/ejb/ServicioFacturacion.java
@@ -47,5 +47,19 @@ public class ServicioFacturacion {
 	public ServicioFacturacion() {
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * Este servicio retorna los datos de la factura
+	 * @param grupos recibe 1 para mostrar todos los materiales y
+	 * @param grupoMaterial recibe 0 para mostrar solo por grupos de acuerdo los ide enviados en el parámetro 
+	 * @return retorna un string con el siguiente contenido (codigo datos factura, autorización 
+	 */
+	public String getDatosFactura(String grupos,String grupoMaterial){
+		String tab_datos_factura="select ide_fadaf,autorizacion_sri_bogrm,serie_factura_fadaf, detalle_bogrm " +
+						" from fac_datos_factura a, bodt_grupo_material b where a.ide_bogrm = b.ide_bogrm ";
+						if(grupos.equals("0")){
+							tab_datos_factura +=" b.ide_bogrm in ("+grupoMaterial+") ";
+						}
+						tab_datos_factura += " order by autorizacion_sri_bogrm";
+		return tab_datos_factura;
+	}
 }
